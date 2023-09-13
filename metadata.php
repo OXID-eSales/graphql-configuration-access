b452826d2ab78c66e0e46171623e5ae3706f8856
marcelmanzel <marcel.manzel@oxid-esales.com>
2023-09-13 16:25:32 +0200

    OXDEV-7350: Add metadata file

---
+++ metadata.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.1';
+
+/**
+ * Module information
+ */
+$aModule = [
+    'id'          => 'oe_graphql_configuration_access',
+    'title'       => 'GraphQL Configuration Access',
+    'description' =>  'GraphQL Configuration Access',
+    'thumbnail'   => 'pictures/logo.png',
+    'version'     => '2.0.0',
+    'author'      => 'OXID eSales AG',
+    'url'         => 'https://github.com/OXID-eSales/graphql-configuration-access',
+    'email'       => 'info@oxid-esales.com',
+    'extend' => [
+    ],
+    'controllers' => [
+    ],
+    'templates' => [
+    ],
+    'events' => [
+    ],
+    'blocks' => [
+    ],
+    'settings' => [
+    ],
+];
