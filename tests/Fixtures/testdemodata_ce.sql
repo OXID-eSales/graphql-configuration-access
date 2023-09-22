3db5387a3072b52c5f40378e4268352074c01fa0
marcelmanzel <marcel.manzel@oxid-esales.com>
2023-09-22 16:39:58 +0200

    OXDEV-7353: Create necessary classes codeception tests

---
+++ tests/Fixtures/testdemodata_ce.sql
@@ -0,0 +1,9 @@
+SET @@session.sql_mode = '';
+
+REPLACE INTO `oxuser` (`OXID`, `OXACTIVE`, `OXRIGHTS`, `OXSHOPID`, `OXUSERNAME`, `OXPASSWORD`, `OXPASSSALT`, `OXCUSTNR`, `OXUSTID`, `OXUSTIDSTATUS`, `OXCOMPANY`, `OXFNAME`, `OXLNAME`, `OXSTREET`, `OXSTREETNR`, `OXADDINFO`, `OXCITY`, `OXCOUNTRYID`, `OXSTATEID`, `OXZIP`, `OXFON`, `OXFAX`, `OXSAL`, `OXBONI`, `OXCREATE`, `OXREGISTER`, `OXPRIVFON`, `OXMOBFON`, `OXBIRTHDATE`, `OXURL`, `OXWRONGLOGINS`, `OXUPDATEKEY`, `OXUPDATEEXP`, `OXPOINTS`, `OXTIMESTAMP`) VALUES
+('4d7a345d24356293cc79b426e6d2c2a6',	1,	'malladmin',	1,	'noreply@oxid-esales.com',	'$2y$10$2JhjaEzBWE1/CbPtgW8CDuXw0pRllIhd50PjFNWsSmGXbh7lrOty6',	'',	1,	'',	0,	'',	'',	'',	'',	'',	'',	'',	'',	'',	'',	'',	'',	'',	0,	'0000-00-00 00:00:00',	'0000-00-00 00:00:00',	'',	'',	'0000-00-00',	'',	0,	'',	0,	0,	'2023-09-13 14:33:18'),
+('01ba0f60-bd8e-4c01-bcf6-aa457bcb',	1,	'user',	1,	'JanvierJaimesVelasquez@cuvox.de',	'$2y$10$xHmv1FlZeE655s8FlZK47ekriYyj81dpq0lrYxVIFLGH2YzovWTXq',	'',	2,	'',	0,	'Omni Realty',	'Janvier',	'Jaimes',	'Wilkinson Street',	'2215',	'',	'Springfield',	'8f241f11096877ac0.98748826',	'',	'37172',	'615-384-2150',	'',	'MR',	1000,	'2023-03-15 08:53:42',	'2023-03-15 08:53:42',	'',	'',	'1998-06-17',	'',	0,	'',	0,	0,	'2023-03-15 08:55:52');
+
+REPLACE INTO `oxgroups` (`OXID`, `OXRRID`, `OXACTIVE`, `OXTITLE`, `OXTITLE_1`, `OXTITLE_2`, `OXTITLE_3`, `OXTIMESTAMP`) VALUES
+('oxidadmin',	9,	1,	'Shop-Admin',	'Store Administrator',	'',	'',	'2023-04-26 10:13:29'),
+('oxidcustomer',	14,	1,	'Kunde',	'Customer',	'',	'',	'2023-04-26 10:13:29');
