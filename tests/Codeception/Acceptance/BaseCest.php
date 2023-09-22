4851aa41ce31c98d33fe1ac5c11f16f68bb52db8
marcelmanzel <marcel.manzel@oxid-esales.com>
2023-09-22 16:40:49 +0200

    OXDEV-7353: WIP: Start first test for the first query

---
+++ tests/Codeception/Acceptance/BaseCest.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\GraphQL\ConfigurationAccess\Tests\Codeception\Acceptance;
+
+use Codeception\Scenario;
+use OxidEsales\EshopCommunity\Internal\Framework\Module\Setup\Exception\ModuleSetupException;
+use OxidEsales\GraphQL\ConfigurationAccess\Tests\Codeception\AcceptanceTester;
+
+abstract class BaseCest
+{
+    public function _before(AcceptanceTester $I, Scenario $scenario): void
+    {
+    }
+
+    public function _after(AcceptanceTester $I): void
+    {
+        $I->logout();
+    }
+}
---
+++ tests/Codeception/Acceptance/SettingCest.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\GraphQL\ConfigurationAccess\Tests\Codeception\Acceptance\Basket;
+
+use OxidEsales\GraphQL\ConfigurationAccess\Tests\Codeception\Acceptance\BaseCest;
+use OxidEsales\GraphQL\ConfigurationAccess\Tests\Codeception\AcceptanceTester;
+
+/**
+ * @group setting
+ * @group setting_access
+ * @group oe_graphql_configuration_access
+ */
+final class SettingCest extends BaseCest
+{
+    private const AGENT_USERNAME = 'JanvierJaimesVelasquez@cuvox.de';
+
+    private const AGENT_PASSWORD = 'agent';
+
+    public function _after(AcceptanceTester $I): void
+    {
+        $I->logout();
+    }
+    public function testGetSettingAuthorized(AcceptanceTester $I): void
+    {
+        $I->login(self::AGENT_USERNAME, self::AGENT_PASSWORD);
+
+        $I->sendGQLQuery(
+            'query{
+                getModuleSettingInteger(name: "iTestConfiguration", moduleId: "oe_graphql_configuration_access") {
+                    name
+                    value
+                }
+            }'
+        );
+
+        $I->seeResponseIsJson();
+
+        $result = $I->grabJsonResponseAsArray();
+        var_dump($result);
+    }
+}
