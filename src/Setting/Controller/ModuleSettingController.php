0f6ae2a24aa1e5694551abee1f2898886fb3c71f
marcelmanzel <marcel.manzel@oxid-esales.com>
2023-10-09 15:35:07 +0200

    OXDEV-7352: Merge ModuleSetting-class into ModuleSettingController.php

--- src/Setting/Controller/ModuleSettingController.php
+++ src/Setting/Controller/ModuleSettingController.php
@@ -9,6 +9,7 @@ use OxidEsales\GraphQL\ConfigurationAccess\Setting\DataType\StringSetting;
 use OxidEsales\GraphQL\ConfigurationAccess\Setting\Service\ModuleSettingServiceInterface;
 use TheCodingMachine\GraphQLite\Annotations\HideIfUnauthorized;
 use TheCodingMachine\GraphQLite\Annotations\Logged;
+use TheCodingMachine\GraphQLite\Annotations\Mutation;
 use TheCodingMachine\GraphQLite\Annotations\Query;
 use TheCodingMachine\GraphQLite\Annotations\Right;
 use TheCodingMachine\GraphQLite\Types\ID;
@@ -72,5 +73,49 @@ final class ModuleSettingController
     public function getModuleSettingCollection(ID $name, string $moduleId): StringSetting
     {
         return $this->settingService->getCollectionSetting($name, $moduleId);
+    }
+
+    /**
+     * @Mutation
+     * @Logged()
+     * @HideIfUnauthorized()
+     * @Right("CHANGE_CONFIGURATION")
+     */
+    public function changeModuleSettingInteger(ID $name, int $value, string $moduleId): IntegerSetting
+    {
+        return $this->settingService->changeIntegerSetting($name, $value, $moduleId);
+    }
+
+    /**
+     * @Mutation
+     * @Logged()
+     * @HideIfUnauthorized()
+     * @Right("CHANGE_CONFIGURATION")
+     */
+    public function changeModuleSettingFloat(ID $name, float $value, string $moduleId): FloatSetting
+    {
+        return $this->settingService->changeFloatSetting($name, $value, $moduleId);
+    }
+
+    /**
+     * @Mutation
+     * @Logged()
+     * @HideIfUnauthorized()
+     * @Right("CHANGE_CONFIGURATION")
+     */
+    public function changeModuleSettingBoolean(ID $name, bool $value, string $moduleId): BooleanSetting
+    {
+        return $this->settingService->changeBooleanSetting($name, $value, $moduleId);
+    }
+
+    /**
+     * @Mutation
+     * @Logged()
+     * @HideIfUnauthorized()
+     * @Right("CHANGE_CONFIGURATION")
+     */
+    public function changeModuleSettingString(ID $name, string $value, string $moduleId): StringSetting
+    {
+        return $this->settingService->changeStringSetting($name, $value, $moduleId);
     }
 }
