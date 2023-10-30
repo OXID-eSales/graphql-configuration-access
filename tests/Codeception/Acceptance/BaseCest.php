a12ed454ce4c6e1e040ca91f5cd254d2d84c41af
marcelmanzel <marcel.manzel@oxid-esales.com>
2023-10-30 16:39:03 +0100

    Revert "OXDEV-7459: Refactor BaseCest and move module methods into ModuleSettingCest"
    This reverts commit f7ead6410afa945ed972973f5eb2628c47afae5a.

--- tests/Codeception/Acceptance/BaseCest.php
+++ tests/Codeception/Acceptance/BaseCest.php
@@ -9,6 +9,12 @@ declare(strict_types=1);
 
 namespace OxidEsales\GraphQL\ConfigurationAccess\Tests\Codeception\Acceptance;
 
+use OxidEsales\EshopCommunity\Internal\Container\ContainerFactory;
+use OxidEsales\EshopCommunity\Internal\Framework\Module\Configuration\Dao\ShopConfigurationDao;
+use OxidEsales\EshopCommunity\Internal\Framework\Module\Configuration\Dao\ShopConfigurationDaoInterface;
+use OxidEsales\EshopCommunity\Internal\Framework\Module\Configuration\DataObject\ModuleConfiguration;
+use OxidEsales\EshopCommunity\Internal\Framework\Module\Configuration\DataObject\ShopConfiguration;
+use OxidEsales\EshopCommunity\Internal\Framework\Module\Setting\Setting;
 use OxidEsales\GraphQL\ConfigurationAccess\Tests\Codeception\AcceptanceTester;
 
 abstract class BaseCest
@@ -16,8 +22,14 @@ abstract class BaseCest
     private const TEST_MODULE_ID = 'awesomeModule';
     private const TEST_THEME_ID = 'awesomeTheme';
 
+    public function _before(AcceptanceTester $I): void
+    {
+        $this->prepareConfiguration();
+    }
+
     public function _after(AcceptanceTester $I): void
     {
+        $this->removeConfiguration(self::TEST_MODULE_ID);
         $I->logout();
     }
 
@@ -29,5 +41,72 @@ abstract class BaseCest
     protected function getTestThemeName(): string
     {
         return self::TEST_THEME_ID;
+    }
+
+    private function prepareConfiguration(): void
+    {
+        $shopConfiguration = $this->getShopConfiguration();
+
+        $integerSetting = new Setting();
+        $integerSetting
+            ->setName('intSetting')
+            ->setValue(123);
+
+        $floatSetting = new Setting();
+        $floatSetting
+            ->setName('floatSetting')
+            ->setValue(1.23);
+
+        $booleanSetting = new Setting();
+        $booleanSetting
+            ->setName('boolSetting')
+            ->setValue(false);
+
+        $stringSetting = new Setting();
+        $stringSetting
+            ->setName('stringSetting')
+            ->setValue('default');
+
+        $collectionSetting = new Setting();
+        $collectionSetting
+            ->setName('arraySetting')
+            ->setValue(['nice', 'values']);
+
+
+        $moduleConfiguration = new ModuleConfiguration();
+        $moduleConfiguration
+            ->setId(self::TEST_MODULE_ID)
+            ->setModuleSource('testPath')
+            ->addModuleSetting($integerSetting)
+            ->addModuleSetting($floatSetting)
+            ->addModuleSetting($booleanSetting)
+            ->addModuleSetting($stringSetting)
+            ->addModuleSetting($collectionSetting);
+
+        $shopConfiguration->addModuleConfiguration($moduleConfiguration);
+        $this->getShopConfigurationDao()->save($shopConfiguration,1);
+    }
+
+    private function removeConfiguration(string $moduleId): void
+    {
+        $shopConfiguration = $this->getShopConfiguration();
+        $shopConfiguration->deleteModuleConfiguration($moduleId);
+
+    }
+
+    private function getShopConfiguration(): ShopConfiguration
+    {
+        $shopConfigurationDao = $this->getShopConfigurationDao();
+        $shopConfiguration = $shopConfigurationDao->get(1);
+
+        return $shopConfiguration;
+    }
+
+    private function getShopConfigurationDao(): ShopConfigurationDao
+    {
+        $container = ContainerFactory::getInstance()->getContainer();
+        /** @var ShopConfigurationDao $shopConfigurationDao */
+        $shopConfigurationDao = $container->get(ShopConfigurationDaoInterface::class);
+        return $shopConfigurationDao;
     }
 }
