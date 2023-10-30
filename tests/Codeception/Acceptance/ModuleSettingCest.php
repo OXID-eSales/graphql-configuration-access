a1caa7fce2047d0b24b9a68d9331768236e1a899
marcelmanzel <marcel.manzel@oxid-esales.com>
2023-10-30 16:39:03 +0100

    Revert "OXDEV-7459: Refactor BaseCest and move module methods into ModuleSettingCest"
    This reverts commit f7ead6410afa945ed972973f5eb2628c47afae5a.

--- tests/Codeception/Acceptance/ModuleSettingCest.php
+++ tests/Codeception/Acceptance/ModuleSettingCest.php
@@ -9,12 +9,6 @@ declare(strict_types=1);
 
 namespace OxidEsales\GraphQL\ConfigurationAccess\Tests\Codeception\Acceptance\Basket;
 
-use OxidEsales\EshopCommunity\Internal\Container\ContainerFactory;
-use OxidEsales\EshopCommunity\Internal\Framework\Module\Configuration\Dao\ShopConfigurationDao;
-use OxidEsales\EshopCommunity\Internal\Framework\Module\Configuration\Dao\ShopConfigurationDaoInterface;
-use OxidEsales\EshopCommunity\Internal\Framework\Module\Configuration\DataObject\ModuleConfiguration;
-use OxidEsales\EshopCommunity\Internal\Framework\Module\Configuration\DataObject\ShopConfiguration;
-use OxidEsales\EshopCommunity\Internal\Framework\Module\Setting\Setting;
 use OxidEsales\GraphQL\ConfigurationAccess\Tests\Codeception\Acceptance\BaseCest;
 use OxidEsales\GraphQL\ConfigurationAccess\Tests\Codeception\AcceptanceTester;
 
@@ -32,17 +26,6 @@ final class ModuleSettingCest extends BaseCest
     private const ADMIN_USERNAME = 'noreply@oxid-esales.com';
 
     private const ADMIN_PASSWORD = 'admin';
-
-    public function _before(AcceptanceTester $I): void
-    {
-        $this->prepareConfiguration();
-    }
-
-    public function _after(AcceptanceTester $I): void
-    {
-        $this->removeConfiguration($this->getTestModuleName());
-        parent::_after($I);
-    }
 
     public function testGetIntegerSettingNotAuthorized(AcceptanceTester $I): void
     {
@@ -472,72 +455,5 @@ final class ModuleSettingCest extends BaseCest
         $setting = $result['data']['changeModuleSettingCollection'];
         $I->assertSame('arraySetting', $setting['name']);
         $I->assertSame('[3, "interesting", "values"]', $setting['value']);
-    }
-
-    private function prepareConfiguration(): void
-    {
-        $shopConfiguration = $this->getShopConfiguration();
-
-        $integerSetting = new Setting();
-        $integerSetting
-            ->setName('intSetting')
-            ->setValue(123);
-
-        $floatSetting = new Setting();
-        $floatSetting
-            ->setName('floatSetting')
-            ->setValue(1.23);
-
-        $booleanSetting = new Setting();
-        $booleanSetting
-            ->setName('boolSetting')
-            ->setValue(false);
-
-        $stringSetting = new Setting();
-        $stringSetting
-            ->setName('stringSetting')
-            ->setValue('default');
-
-        $collectionSetting = new Setting();
-        $collectionSetting
-            ->setName('arraySetting')
-            ->setValue(['nice', 'values']);
-
-
-        $moduleConfiguration = new ModuleConfiguration();
-        $moduleConfiguration
-            ->setId($this->getTestModuleName())
-            ->setModuleSource('testPath')
-            ->addModuleSetting($integerSetting)
-            ->addModuleSetting($floatSetting)
-            ->addModuleSetting($booleanSetting)
-            ->addModuleSetting($stringSetting)
-            ->addModuleSetting($collectionSetting);
-
-        $shopConfiguration->addModuleConfiguration($moduleConfiguration);
-        $this->getShopConfigurationDao()->save($shopConfiguration,1);
-    }
-
-    private function removeConfiguration(string $moduleId): void
-    {
-        $shopConfiguration = $this->getShopConfiguration();
-        $shopConfiguration->deleteModuleConfiguration($moduleId);
-
-    }
-
-    private function getShopConfiguration(): ShopConfiguration
-    {
-        $shopConfigurationDao = $this->getShopConfigurationDao();
-        $shopConfiguration = $shopConfigurationDao->get(1);
-
-        return $shopConfiguration;
-    }
-
-    private function getShopConfigurationDao(): ShopConfigurationDao
-    {
-        $container = ContainerFactory::getInstance()->getContainer();
-        /** @var ShopConfigurationDao $shopConfigurationDao */
-        $shopConfigurationDao = $container->get(ShopConfigurationDaoInterface::class);
-        return $shopConfigurationDao;
     }
 }
