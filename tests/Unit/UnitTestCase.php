9fc26991dd415e9028f4f796b63bb97c6deca23a
Anton Fedurtsya <anton@fedurtsya.com>
2023-12-01 16:35:29 +0200

    OXDEV-7456 Cleanup not used methods from UnitTestCase
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

--- tests/Unit/UnitTestCase.php
+++ tests/Unit/UnitTestCase.php
@@ -22,60 +22,6 @@ use TheCodingMachine\GraphQLite\Types\ID;
 
 class UnitTestCase extends TestCase
 {
-    protected function getIntegerSetting(): IntegerSetting
-    {
-        return new IntegerSetting(new ID('integerSetting'), 123);
-    }
-
-    protected function getFloatSetting(): FloatSetting
-    {
-        return new FloatSetting(new ID('floatSetting'), 1.23);
-    }
-
-    protected function getNegativeBooleanSetting(): BooleanSetting
-    {
-        return new BooleanSetting(new ID('booleanSetting'), false);
-    }
-
-    protected function getPositiveBooleanSetting(): BooleanSetting
-    {
-        return new BooleanSetting(new ID('booleanSetting'), true);
-    }
-
-    protected function getStringSetting(): StringSetting
-    {
-        return new StringSetting(new ID('stringSetting'), 'default');
-    }
-
-    protected function getSelectSetting(): StringSetting
-    {
-        return new StringSetting(new ID('selectSetting'), 'select');
-    }
-
-    protected function getCollectionSetting(): StringSetting
-    {
-        return new StringSetting(new ID('arraySetting'), json_encode(['nice', 'values']));
-    }
-
-    protected function getAssocCollectionSetting(): StringSetting
-    {
-        return new StringSetting(
-            new ID('aarraySetting'),
-            json_encode(
-                [
-                    'first' => '10',
-                    'second' => '20',
-                    'third' => '50'
-                ]
-            )
-        );
-    }
-
-    protected function getSettingType(): SettingType
-    {
-        return new SettingType(new ID('settingType'), FieldType::BOOLEAN);
-    }
-
     /**
      * @return SettingType[]
      */
