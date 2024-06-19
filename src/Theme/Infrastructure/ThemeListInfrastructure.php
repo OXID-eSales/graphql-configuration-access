f99ed968b571b55e05f87956ef0abd5a9828f2cf
RahatHameed <the_rahat_online@yahoo.com>
2024-06-19 16:43:45 +0200

    OXDEV-8213 Improved exception name

--- src/Theme/Infrastructure/ThemeListInfrastructure.php
+++ src/Theme/Infrastructure/ThemeListInfrastructure.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
 namespace OxidEsales\GraphQL\ConfigurationAccess\Theme\Infrastructure;
 
 use OxidEsales\GraphQL\ConfigurationAccess\Theme\DataType\ThemeDataTypeFactoryInterface;
-use OxidEsales\GraphQL\ConfigurationAccess\Theme\Exception\ThemeNotFound;
+use OxidEsales\GraphQL\ConfigurationAccess\Theme\Exception\ThemesNotFound;
 
 final class ThemeListInfrastructure implements ThemeListInfrastructureInterface
 {
@@ -20,19 +20,20 @@ final class ThemeListInfrastructure implements ThemeListInfrastructureInterface
     ) {
     }
 
-
     public function getThemes(): array
     {
         $coreThemeService = $this->coreThemeFactory->getClass();
         $themesList = $coreThemeService->getList();
 
+        if (empty($themesList)) {
+            throw new ThemesNotFound();
+        }
+
         $themesArray = [];
         foreach ($themesList as $theme) {
             $themesArray[] = $this->themeDataTypeFactory->createFromCoreTheme(theme: $theme);
         }
-        if (empty($themesList)) {
-            throw new ThemeNotFound();
-        }
+
         return $themesArray;
     }
 }
