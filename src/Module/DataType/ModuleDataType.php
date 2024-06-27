8cabc3bd8e56a35130dbb6d5392cc08b04451d2e
RahatHameed <the_rahat_online@yahoo.com>
2024-06-27 12:07:27 +0200

    OXDEV-8215 Prepared  ModuleDataType

---
+++ src/Module/DataType/ModuleDataType.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\GraphQL\ConfigurationAccess\Module\DataType;
+
+use TheCodingMachine\GraphQLite\Annotations\Field;
+use TheCodingMachine\GraphQLite\Annotations\Type;
+
+#[Type]
+class ModuleDataType
+{
+    public function __construct(
+        private readonly string $id,
+        private readonly ?string $version,
+        private readonly ?string $title,
+        private readonly ?string $description,
+        private readonly ?string $lang,
+        private readonly ?string $thumbnail,
+        private readonly ?string $author,
+        private readonly ?string $url,
+        private readonly ?string $email,
+        private readonly bool $active
+    ) {
+    }
+
+    #[Field]
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    #[Field]
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    #[Field]
+    public function getVersion(): ?string
+    {
+        return $this->version;
+    }
+
+    #[Field]
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    #[Field]
+    public function getThumbnail(): ?string
+    {
+        return $this->thumbnail;
+    }
+
+    #[Field]
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    #[Field]
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    #[Field]
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    #[Field]
+    public function getLang(): ?string
+    {
+        return $this->lang;
+    }
+
+    #[Field]
+    public function isActive(): bool
+    {
+        return $this->active;
+    }
+}
---
+++ tests/Unit/Module/DataType/ModuleDataTypeTest.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\GraphQL\ConfigurationAccess\Tests\Unit\Module\DataType;
+
+use OxidEsales\GraphQL\ConfigurationAccess\Module\DataType\ModuleDataType;
+use OxidEsales\GraphQL\ConfigurationAccess\Tests\Unit\UnitTestCase;
+
+/**
+ * @covers \OxidEsales\GraphQL\ConfigurationAccess\Module\DataType\ModuleDataType
+ */
+class ModuleDataTypeTest extends UnitTestCase
+{
+    public function testModuleDataType(): void
+    {
+        $title = uniqid();
+        $id = uniqid();
+        $version = uniqid();
+        $description = uniqid();
+        $lang = uniqid();
+        $thumbnail = uniqid();
+        $author = uniqid();
+        $url = uniqid();
+        $email = uniqid();
+        $active = true;
+
+        $moduleDataType = new ModuleDataType(
+            id: $id,
+            version: $version,
+            title: $title,
+            description: $description,
+            lang: $lang,
+            thumbnail: $thumbnail,
+            author: $author,
+            url: $url,
+            email: $email,
+            active: $active
+        );
+
+        $this->assertSame($id, $moduleDataType->getId());
+        $this->assertSame($version, $moduleDataType->getVersion());
+        $this->assertSame($title, $moduleDataType->getTitle());
+        $this->assertSame($description, $moduleDataType->getDescription());
+        $this->assertSame($lang, $moduleDataType->getLang());
+        $this->assertSame($thumbnail, $moduleDataType->getThumbnail());
+        $this->assertSame($author, $moduleDataType->getAuthor());
+        $this->assertSame($url, $moduleDataType->getUrl());
+        $this->assertSame($email, $moduleDataType->getEmail());
+        $this->assertSame($active, $moduleDataType->isActive());
+    }
+}
