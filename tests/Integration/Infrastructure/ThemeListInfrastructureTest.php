a2dd54785eaf8d524841436cd7693bf221f3db09
marcelmanzel <marcel.manzel@oxid-esales.com>
2024-06-28 16:34:19 +0200

    OXDEV-8213: Remove already tested "Integration test"

--- tests/Integration/Infrastructure/ThemeListInfrastructureTest.php
+++ tests/Integration/Infrastructure/ThemeListInfrastructureTest.php
@@ -36,19 +36,6 @@ class ThemeListInfrastructureTest extends IntegrationTestCase
         }
     }
 
-    public function testGetThemesThrowsException(): void
-    {
-        $coreThemeStub = $this->createMock(Theme::class);
-        $coreThemeStub->method('getList')
-            ->willReturn([]);
-        $coreThemeFactoryMock = $this->getCoreThemeFactoryMock($coreThemeStub);
-
-        $sut = $this->getSut(coreThemeFactory: $coreThemeFactoryMock);
-
-        $this->expectException(ThemesNotFound::class);
-        $sut->getThemes();
-    }
-
     public function getSut(
         ?CoreThemeFactoryInterface $coreThemeFactory = null,
         ?ThemeDataTypeFactoryInterface $themeDataTypeFactory = null
@@ -57,15 +44,5 @@ class ThemeListInfrastructureTest extends IntegrationTestCase
             $coreThemeFactory ?? $this->get(CoreThemeFactoryInterface::class),
             $themeDataTypeFactory ?? $this->get(ThemeDataTypeFactoryInterface::class)
         );
-    }
-
-    private function getCoreThemeFactoryMock(mixed $returnValue): CoreThemeFactoryInterface
-    {
-        $coreThemeFactoryMock = $this->createMock(CoreThemeFactoryInterface::class);
-        $coreThemeFactoryMock->expects($this->once())
-            ->method('getClass')
-            ->willReturn($returnValue);
-
-        return $coreThemeFactoryMock;
     }
 }
