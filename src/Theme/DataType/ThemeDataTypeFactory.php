ec47367fb36788dfb9cfe7959562e426f6a841fc
marcelmanzel <marcel.manzel@oxid-esales.com>
2024-07-18 15:13:39 +0200

    Revert "Revert "OXDEV-8213 Added theme DataType factory""
    This reverts commit 499c247fa07d4dd91929b4e06d1578ba40fe2235.

---
+++ src/Theme/DataType/ThemeDataTypeFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\GraphQL\ConfigurationAccess\Theme\DataType;
+
+use OxidEsales\Eshop\Core\Theme;
+
+class ThemeDataTypeFactory implements ThemeDataTypeFactoryInterface
+{
+    public function createFromCoreTheme(
+        Theme $theme
+    ): ThemeDataType {
+        return new ThemeDataType(
+            title: $theme->getInfo('title'),
+            identifier: $theme->getInfo('id'),
+            version: $theme->getInfo('version'),
+            description: $theme->getInfo('description'),
+            active: $theme->getInfo('active')
+        );
+    }
+}
---
+++ src/Theme/DataType/ThemeDataTypeFactoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace OxidEsales\GraphQL\ConfigurationAccess\Theme\DataType;
+
+use OxidEsales\Eshop\Core\Theme;
+
+interface ThemeDataTypeFactoryInterface
+{
+    public function createFromCoreTheme(Theme $theme): ThemeDataType;
+}
