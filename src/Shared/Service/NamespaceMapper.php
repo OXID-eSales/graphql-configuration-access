4808c35d91b0406c0edddf5f8aee47cf519fb09c
marcelmanzel <marcel.manzel@oxid-esales.com>
2024-07-18 15:13:35 +0200

    Revert "Revert "OXDEV-8213 Added TheDataType""
    This reverts commit a902feb57d1e7b540c68a93f4cd1a3e632c69617.

--- src/Shared/Service/NamespaceMapper.php
+++ src/Shared/Service/NamespaceMapper.php
@@ -28,6 +28,7 @@ final class NamespaceMapper implements NamespaceMapperInterface
     {
         return [
             self::SPACE . 'Shared\\DataType' => __DIR__ . '/../../Shared/DataType/',
+            self::SPACE . 'Theme\\DataType' => __DIR__ . '/../../Theme/DataType/',
         ];
     }
 }
