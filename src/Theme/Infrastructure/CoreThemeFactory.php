98e2609cba127d7b3a96e0020a7d9486352856d5
marcelmanzel <marcel.manzel@oxid-esales.com>
2024-07-18 15:13:38 +0200

    Revert "Revert "OXDEV-8213 Renamed service to theme specific""
    This reverts commit 86205f2cdec4a6a5b752b881f17bb074ce783981.

---
+++ src/Theme/Infrastructure/CoreThemeFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\GraphQL\ConfigurationAccess\Theme\Infrastructure;
+
+use OxidEsales\Eshop\Core\Theme;
+
+class CoreThemeFactory implements CoreThemeFactoryInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function getClass(): Theme
+    {
+        return oxNew(Theme::class);
+    }
+}
