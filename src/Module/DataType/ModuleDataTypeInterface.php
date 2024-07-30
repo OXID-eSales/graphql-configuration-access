b86469d764f0b7a60b980fb436366f3126d09a54
RahatHameed <the_rahat_online@yahoo.com>
2024-07-30 13:24:59 +0200

    OXDEV-8215 Introduced ModuleDataTypeInterface

---
+++ src/Module/DataType/ModuleDataTypeInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace OxidEsales\GraphQL\ConfigurationAccess\Module\DataType;
+
+interface ModuleDataTypeInterface
+{
+    public function getId(): string;
+
+    public function getTitle(): ?string;
+
+    public function getVersion(): ?string;
+
+    public function getDescription(): ?string;
+
+    public function getThumbnail(): ?string;
+
+    public function getAuthor(): ?string;
+
+    public function getUrl(): ?string;
+
+    public function getEmail(): ?string;
+
+    public function isActive(): bool;
+}
