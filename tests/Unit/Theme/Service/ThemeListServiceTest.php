62b6a4079d662e934ac64bcb9ae4db2ed835628c
RahatHameed <the_rahat_online@yahoo.com>
2024-07-30 11:16:53 +0200

    Move ThemeDataType creation into service

--- tests/Unit/Theme/Service/ThemeListServiceTest.php
+++ tests/Unit/Theme/Service/ThemeListServiceTest.php
@@ -12,11 +12,13 @@ namespace OxidEsales\GraphQL\ConfigurationAccess\Tests\Unit\Theme\Service;
 use OxidEsales\GraphQL\Base\DataType\Filter\BoolFilter;
 use OxidEsales\GraphQL\Base\DataType\Filter\StringFilter;
 use OxidEsales\GraphQL\ConfigurationAccess\Theme\DataType\ThemeDataType;
+use OxidEsales\GraphQL\ConfigurationAccess\Theme\DataType\ThemeDataTypeFactoryInterface;
 use OxidEsales\GraphQL\ConfigurationAccess\Theme\DataType\ThemeFilters;
 use OxidEsales\GraphQL\ConfigurationAccess\Theme\Infrastructure\ThemeListInfrastructureInterface;
 use OxidEsales\GraphQL\ConfigurationAccess\Theme\Service\ThemeFilterServiceInterface;
 use OxidEsales\GraphQL\ConfigurationAccess\Theme\Service\ThemeListService;
 use PHPUnit\Framework\TestCase;
+use OxidEsales\Eshop\Core\Theme;
 
 /**
  * @covers \OxidEsales\GraphQL\ConfigurationAccess\Theme\Service\ThemeListService
@@ -25,6 +27,7 @@ class ThemeListServiceTest extends TestCase
 {
     public function testGetThemeListWithFilters(): void
     {
+        $themes = [new Theme(), new Theme()];
         $theme1 = new ThemeDataType(uniqid(), uniqid(), uniqid(), uniqid(), true);
         $theme2 = new ThemeDataType(uniqid(), uniqid(), uniqid(), uniqid(), false);
         $themeList = [$theme1, $theme2];
@@ -32,7 +35,12 @@ class ThemeListServiceTest extends TestCase
 
         $themeListInfrastructureMock = $this->createMock(ThemeListInfrastructureInterface::class);
         $themeListInfrastructureMock->expects($this->once())->method('getThemes')
-            ->willReturn($themeList);
+            ->willReturn($themes);
+
+        $themeDataTypeFactoryMock = $this->createMock(ThemeDataTypeFactoryInterface::class);
+        $themeDataTypeFactoryMock
+            ->method('createFromCoreTheme')
+            ->willReturnOnConsecutiveCalls($themeList[0], $themeList[1]);
 
         $filtersList = new ThemeFilters(
             titleFilter: new StringFilter(contains: uniqid()),
@@ -46,7 +54,8 @@ class ThemeListServiceTest extends TestCase
 
         $themeListService = new ThemeListService(
             themeListInfrastructure: $themeListInfrastructureMock,
-            themeFilterService:  $themeFilterServiceMock
+            themeFilterService:  $themeFilterServiceMock,
+            themeDataTypeFactory:  $themeDataTypeFactoryMock
         );
         $actualThemes = $themeListService->getThemeList($filtersList);
         $this->assertSame($filteredThemeList, $actualThemes);
